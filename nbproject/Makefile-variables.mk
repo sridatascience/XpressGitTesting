b5ec0381fa0ce360f42516d308dc9229db034ee5
sridharmicrochip@gmail.com <sridharmicrochip@gmail.com>
2022-12-16 05:31:42 +0000

    Project created

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PIC24FJ1024GB610.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PIC24FJ1024GB610.production.hex
