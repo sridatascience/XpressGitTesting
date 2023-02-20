b547df4fe1304ca857b752a152cee472ed7b46ae
sridharmanual@gmail.com <sridharmanual@gmail.com>
2023-02-20 07:17:46 +0000

    local comment added on development1 branch before committing the code

--- newmain.c
+++ newmain.c
@@ -24,4 +24,4 @@ void main() {
 //End Comment -3
 //Code update done on 20-2-2023 from local repository
 //Code update done on 20-2-2023 from remote repository and committed in remote repository only
-
+//Local Changes Made on 20-2-2023 in development1 branch
